5c96b89f7a160d21669029b230f057082c3b558e
Sergey <workintellect@gmail.com>
2018-09-23 16:44:12 +0300

    Money-Exchange/implemented function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    var balance, H=50, Q=25, D=10, N=5, P=1, countH, countQ, countD, countN, countP, obj = {};
+
+    if(currency <= 0) {
+        return obj;
+    }else if(currency > 9999) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }else{
+        countH = Math.floor(currency/H);
+        if(countH > 0) {
+            obj["H"] = countH;
+        }
+        balance = currency%H;
+        if(balance >= Q) {
+            countQ = Math.floor(balance / Q);
+            balance = balance%Q;
+            obj["Q"] = countQ;
+        }
+        if(balance >= D) {
+            countD = Math.floor(balance/D );
+            balance = balance%D;
+            obj["D"] = countD;
+        }
+        if(balance >= N) {
+            countN = Math.floor(balance/N );
+            balance = balance%N;
+            obj["N"] = countN;
+        }
+        if(balance >= P) {
+            countP = Math.floor(balance/P );
+            balance = balance%P;
+            obj["P"] = countP;
+        }
+        return obj;
+    }
 }
